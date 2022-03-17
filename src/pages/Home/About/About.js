b3bd0a69fc4ff5eb9363e1443428f16cd827e127
rimonspi12 <rimonmpi@gmail.com>
2022-03-17 18:21:01 +0600

    profile images added

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import profile from '../../../images/abou_me.jpg';
+// import profile from '../../../images/abou_me.jpg';
 // import Zoom from 'react-reveal/Zoom';
 import './About.css';
 
 const About = () => {
     return (
         <div className='container' style={{ marginTop: '25vh' }}>
-            <div class="row g-5">
+            <div class="row g-4">
                 <div className='col-sm-12 col-md-6 col-lg-6'>
                     <p style={{ textAlign: 'justify' }}>
-                        <img src={profile} alt="profile-images" className='img-fluid' />
+                        <img id="profile-img" className='img-fluid' src="https://i.ibb.co/R7pX5yv/74418535.jpg" alt="74418535" border="0"/>
+                        {/* <img src={profile} alt="profile-images" className='img-fluid' /> */}
                     </p>
                 </div>
                 <div className='col-sm-12 col-md-6 col-lg-6'>
-                    <p style={{ textAlign: 'justify', marginTop:'8vh',fontSize:'18px' }}>
+                    <p style={{ textAlign: 'justify', marginTop:'8vh',fontSize:'17px' }}>
                         Hi! I am Rimon Hossain, a 4th year student of Diploma in Computer Science and Technology. I am a Marn Stack developer as well as a programming problem solving lover. I am constantly improving myself to improve myself in this field.</p>
                 </div>
             </div>
