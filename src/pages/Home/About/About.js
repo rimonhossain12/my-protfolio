a2ec87fcec7701d18bd7bac184c6752ba688971a
rimonspi12 <rimonmpi@gmail.com>
2022-03-17 18:14:00 +0600

    about me section added

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import profile from '../../../images/abou_me.jpg';
+// import Zoom from 'react-reveal/Zoom';
+import './About.css';
 
 const About = () => {
     return (
-        <div className='container' style={{marginTop:'10vh'}}>
-            <div>
-                <h2 className='text-start'>About Me</h2>
-                <div className="row mt-3">
-                    <div className="col-sm-6 col-md-6 col-lg-6">
-                        <img src={profile} alt="profile images" className='img-fluid rounded' />
-                    </div>
-                    <div className="col-sm-6 col-md-6 col-lg-6">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consequuntur quasi facere architecto, ipsam molestiae maxime doloribus laudantium ducimus ipsum.
-                    </div>
+        <div className='container' style={{ marginTop: '25vh' }}>
+            <div class="row g-5">
+                <div className='col-sm-12 col-md-6 col-lg-6'>
+                    <p style={{ textAlign: 'justify' }}>
+                        <img src={profile} alt="profile-images" className='img-fluid' />
+                    </p>
+                </div>
+                <div className='col-sm-12 col-md-6 col-lg-6'>
+                    <p style={{ textAlign: 'justify', marginTop:'8vh',fontSize:'18px' }}>
+                        Hi! I am Rimon Hossain, a 4th year student of Diploma in Computer Science and Technology. I am a Marn Stack developer as well as a programming problem solving lover. I am constantly improving myself to improve myself in this field.</p>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
