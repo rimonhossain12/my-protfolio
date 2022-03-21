cf18817d539879e858e521b3b23d5625711ab176
rimonspi12 <rimonmpi@gmail.com>
2022-03-21 21:22:29 +0600

    instagram link added

--- src/pages/Home/Footer/Footer.js
+++ src/pages/Home/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsGithub, BsFacebook, BsLinkedin } from "react-icons/bs";
+import { BsGithub, BsFacebook, BsLinkedin, BsWhatsapp, BsInstagram } from "react-icons/bs";
 import './Footer.css';
 
 const Footer = () => {
@@ -16,9 +16,14 @@ const Footer = () => {
                 <a href="https://www.linkedin.com/in/rimonhossain12/" target={`_blank`}>
                     <BsLinkedin className='social-icon'/>
                 </a>
-
+                <a href="#home">
+                    <BsWhatsapp className='social-icon'/>
+                </a>
+                <a href="https://www.instagram.com/ar.rimon.7731/" target={`_blank`}>
+                    <BsInstagram className='social-icon' />
+                </a>
             </div>
-            <p>All rights reversed &copy; Rimon</p>
+            <p style={{fontSize:'17px',padding:'5px'}}>All rights reversed &copy; Rimon</p>
         </div>
     );
 };
