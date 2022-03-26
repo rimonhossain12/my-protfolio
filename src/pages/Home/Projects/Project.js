cf04f0149e4af914ce6c9987b751e228bdae7b14
rimonspi12 <rimonmpi@gmail.com>
2022-03-26 18:49:08 +0600

    linkup project live and github url

--- src/pages/Home/Projects/Project.js
+++ src/pages/Home/Projects/Project.js
@@ -10,7 +10,7 @@ const Project = () => {
             <h2>This is my projects</h2>
             <Container>
                 <Row className="g-4">
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/QvzJ79Y/Screenshot-54.png'} />
                             <div className='mt-2'>
@@ -23,10 +23,20 @@ const Project = () => {
                                 <span>Bootstrap5</span>
                                 <span>JS</span>
                             </div>
+                            <ul className='mt-2'>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/computer-programmer-websit" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://blissful-gates-0f8c51.netlify.app/" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
 
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/k0M4hKM/Screenshot-55.png'} />
                             <div className='mt-2'>
@@ -40,9 +50,19 @@ const Project = () => {
                                 <span>JS</span>
                                 <span>ES6</span>
                             </div>
+                            <ul className='mt-2'>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/Javasript-Mac-Book-project" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://youthful-liskov-293233.netlify.app/" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/rsykcM1/Screenshot-60.png'} />
                             <div className='mt-2'>
@@ -53,12 +73,22 @@ const Project = () => {
                                 <span>Html</span>
                                 <span>Css</span>
                                 <span>Bootstrap5</span>
-                                <span>React</span> <br />
+                                <span>React</span>
                                 <span>Firebase</span>
                             </div>
+                            <ul className='mt-2'>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/medical-website-firebase" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://medical-firebase-project.web.app/" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/hXJY997/Screenshot-63.png'} />
                             <div className='mt-2'>
@@ -72,9 +102,19 @@ const Project = () => {
                                 <span>React</span>
                                 <span>REST API</span>
                             </div>
+                            <ul className='mt-2'>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/theMealDb-with-react" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://clever-dubinsky-af8a8d.netlify.app/home" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/Gd52kfv/travel-site-1.png'} />
                             <div className='mt-2'>
@@ -89,9 +129,19 @@ const Project = () => {
                                 <span>Firebase</span>
                                 <span>Bootstrap5</span>
                             </div>
+                            <ul>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/tourism-or-delivery-website-client-side" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://travelling-blog-site.web.app/" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
-                    <Col xs={1} md={2} lg={3}>
+                    <Col xs={12} md={6} lg={4}>
                         <Card className="project-card">
                             <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/5L8bRtT/Screenshot-51.png'} />
                             <div className='mt-2'>
@@ -104,8 +154,18 @@ const Project = () => {
                                 <span>Express.js</span>
                                 <span>Mongodb</span>
                                 <span>Firebase</span>
-                                <span>Bootstrap5</span>
+                                <span>bootstrap</span>
                             </div>
+                            <ul>
+                                <li>
+                                    <a href="https://github.com/rimonhossain12/bycycle-web-site-client" target={`_blank`}>
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                    <a href="https://bicycle-application.web.app/" target={`_blank`}>
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
                 </Row>
