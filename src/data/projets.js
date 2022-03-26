c142620d0b286efb4439778248dfdf7c62701127
rimonspi12 <rimonmpi@gmail.com>
2022-03-26 18:12:45 +0600

    added my project

---
+++ src/data/projets.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+        id:'1',
+        Name:'ByCycle website',
+        img:'https://i.ibb.co/5L8bRtT/Screenshot-51.png'
+    }
+    ,
+    {
+        id:'2',
+        Name:'World Top programer',
+        img:'https://i.ibb.co/QvzJ79Y/Screenshot-54.png'
+    },
+    {
+        id:'3',
+        Name:'Mac Book project',
+        img:'https://i.ibb.co/k0M4hKM/Screenshot-55.png'
+    }
+    ,{
+        id:'4',
+        Name:'ES6 Modern project',
+        img:'https://i.ibb.co/D54fMdX/Screenshot-56.png'
+    },
+    {
+        id:'5',
+        Name:'React Medical Project',
+        img:'https://i.ibb.co/rsykcM1/Screenshot-60.png'
+    },
+    {
+        id:'6',
+        Name:'The MealDb',
+        img:'https://i.ibb.co/hXJY997/Screenshot-63.png'
+    }
+    ,
+    {
+        id:'7',
+        Name:'Travel Project',
+        img:'https://i.ibb.co/Gd52kfv/travel-site-1.png',
+        // Technologies = ['React.js', 'React Bootstrap', 'Firebase', 'Node.js', 'Express.js', 'Mongodb', 'Heroku'],
+    }
+];
+
+// export const BicycleTechnologies = ['React.js','React Bootstrap','Firebase','Node.js','Express.js','Mongodb','Heroku'];
+
+
+export const skill = [
+    {
+        id:'1',
+        technologies:''
+    }
+]
--- src/pages/Home/Projects/Project.js
+++ src/pages/Home/Projects/Project.js
@@ -1,30 +1,115 @@
 // import { Button } from 'bootstrap';
 import React from 'react';
-import { Card, Col, Container,Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+
 import './Project.css';
 
 const Project = () => {
     return (
         <div style={{ marginTop: '15vh' }}>
             <h2>This is my projects</h2>
-           <Container>
-                <Row xs={1} md={2} lg={3} className="g-4">
-                    {Array.from({ length: 6 }).map((_, idx) => (
-                        <Col>
-                            <Card className="project-card">
-                                <Card.Img variant="top" src="holder.js/100px160" />
-                                <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text>
-                                        This is a longer card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
+            <Container>
+                <Row className="g-4">
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/QvzJ79Y/Screenshot-54.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">world top programmer</Card.Title>
+                                <p className='text-start'>This is world most talented programmer website. who are making various programming languages. And making important things for modern technologies </p>
+                            </div>
+                            <div className='technology'>
+                                <span>Html</span>
+                                <span>Css</span>
+                                <span>Bootstrap5</span>
+                                <span>JS</span>
+                            </div>
+                        </Card>
+                    </Col>
+
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/k0M4hKM/Screenshot-55.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">Mac Book project</Card.Title>
+                                <p className='text-start'>This is mac book project user can click any product button then price added the card.After click all the button users see the total product price. If user have any promo code then type promo code users will be discount product price.</p>
+                            </div>
+                            <div className='technology'>
+                                <span>Html</span>
+                                <span>Css</span>
+                                <span>Bootstrap5</span>
+                                <span>JS</span>
+                                <span>ES6</span>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/rsykcM1/Screenshot-60.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">React static medical project</Card.Title>
+                                <p className='text-start'>It's is React static medical projects. users can see the home page all service doctors list.</p>
+                            </div>
+                            <div className='technology'>
+                                <span>Html</span>
+                                <span>Css</span>
+                                <span>Bootstrap5</span>
+                                <span>React</span> <br />
+                                <span>Firebase</span>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/hXJY997/Screenshot-63.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">The MealDb</Card.Title>
+                                <p className='text-start'>It's is React static food project.</p>
+                            </div>
+                            <div className='technology'>
+                                <span>Html</span>
+                                <span>Css</span>
+                                <span>Bootstrap5</span>
+                                <span>React</span>
+                                <span>REST API</span>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/Gd52kfv/travel-site-1.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">Travel web site</Card.Title>
+                                <p className='text-start'>React Travel web site. user booking any place. DashBoard page manage all the booking place and cancel tour.</p>
+                            </div>
+                            <div className='technology'>
+                                <span>React.js</span>
+                                <span>Node.js</span>
+                                <span>Express.js</span>
+                                <span>Mongodb</span>
+                                <span>Firebase</span>
+                                <span>Bootstrap5</span>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs={1} md={2} lg={3}>
+                        <Card className="project-card">
+                            <Card.Img className='img-fluid mb-1' variant="top" src={'https://i.ibb.co/5L8bRtT/Screenshot-51.png'} />
+                            <div className='mt-2'>
+                                <Card.Title className="text-start mt-2">Rect ByCycle webpage</Card.Title>
+                                <p className='text-start'>React ByCycle website. users buy any product and see his all product in dashboard pages. Admin can manage all the orders.</p>
+                            </div>
+                            <div className='technology'>
+                                <span>React.js</span>
+                                <span>Node.js</span>
+                                <span>Express.js</span>
+                                <span>Mongodb</span>
+                                <span>Firebase</span>
+                                <span>Bootstrap5</span>
+                            </div>
+                        </Card>
+                    </Col>
                 </Row>
-           </Container>
+            </Container>
         </div>
     );
 };
